d610f64f25d9e01430cb576fc1c3937add1b33cc
worms308 <worms308@wp.pl>
2020-03-21 13:18:10 +0100

    Init backend

---
+++ Backend/src/main/java/com/farmclick/FarmclickApplication.java
@@ -0,0 +1,13 @@
+package com.farmclick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmclickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FarmclickApplication.class, args);
+    }
+
+}
---
+++ Backend/src/test/java/com/farmclick/FarmclickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmclick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmclickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
