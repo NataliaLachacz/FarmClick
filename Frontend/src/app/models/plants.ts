2038dbcf314eca1cf1185e8621925d921d97e366
Natalia Łachacz <natalia.lachacz@gmail.com>
2021-03-20 19:44:54 +0100

    scale plant when it click

---
+++ Frontend/src/app/models/plants.ts
@@ -0,0 +1,27 @@
+export const plants = [
+  {
+    name: 'wheat',
+    value: 5,
+    toUnlock: 0
+  },
+  {
+    name: 'carrot',
+    value: 15,
+    toUnlock: 5000
+  },
+  {
+    name: 'tomato',
+    value: 35,
+    toUnlock: 10000
+  },
+  {
+    name: 'cucumber',
+    value: 65,
+    toUnlock: 25000
+  },
+  {
+    name: 'pumpkin',
+    value: 80,
+    toUnlock: 50000
+  }
+]
