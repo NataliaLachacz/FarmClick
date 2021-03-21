8d52f5c997242c013cf0ac907ca17834cb6d59c0
Patrycjusz Mania <worms308@wp.pl>
2021-03-21 14:45:59 +0100

    hotfix start.bat

--- Backend/start.bat
+++ Backend/start.bat
@@ -1,2 +1,2 @@
-start java -jar MS-API/target/micro-service-api-0.0.1-SNAPSHOT.jar
-start java -jar MS-Authorization/target/micro-service-authorization-0.0.1-SNAPSHOT.jar
+start java -jar farm-click-api/target/farm-click-api-0.0.1-SNAPSHOT.jar
+start java -jar farm-click-auth/target/farm-click-auth-0.0.1-SNAPSHOT.jar
