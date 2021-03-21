816807c80272aa39fdcc940267bbdde1b08d3ee2
Worms308 <39744125+Worms308@users.noreply.github.com>
2021-03-21 19:08:08 +0100

    added auth test profile with first test (#19)

---
+++ Backend/farm-click-auth/src/test/java/com/farmclick/TestBase.java
@@ -0,0 +1,15 @@
+package com.farmclick;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = AuthorizationApplication.class)
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application-test.properties")
+public class TestBase {
+
+}
